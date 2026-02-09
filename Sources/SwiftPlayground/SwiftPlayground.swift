8647b4f47b2741da9d5ffa0713de133518be15af
TorinPainter <torin.painter@student.onslow.school.nz>
2026-02-09 02:06:58 +0000

    Doing task 1

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,14 +4,25 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches: [Float] = [6.50, 8.00, 5.75, 9.20, 7.10]
+        let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
+        let budget: Double = 35
 
         lunches.enumerated().forEach {idx, lunchcost in 
             print("Day \(idx + 1): $\(lunchcost)")
         }
+        print(averageCost(prices: lunches))
     }
 }
 
 func isOverBudget(total: Double, budget: Double) -> Bool {
     return total > budget
+}
+
+func averageCost(prices: [Double]) -> Double {
+    var total: Double = 0
+    for price in prices {
+        total += price
+    }
+
+    return total / Double(prices.count + 1)
 }
