d2d4ed2b151625e9fb3b23cc77035063d6113336
TorinPainter <torin.painter@student.onslow.school.nz>
2026-02-10 23:28:03 +0000

    Learning the swift magic

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,8 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, World!")
+        print([1, 2, 5, 1, 3, 2, 4, 5, 2, 1, 1, 1, 5].filter({$0 % 2 == 0})) // Filtered
+        print([1, 7, 3, 9].map({$0 * $0})) // Squared
+        print([1, 4, 7, 2].reduce(0, {$0 + $1})) // Sum
     }
 }
