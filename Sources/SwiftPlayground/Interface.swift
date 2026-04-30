b3447ed7503a8bef27b88a3823722d203d16357f
W0lfPlay <tiredgamer.torin@gmail.com>
2026-05-01 11:14:44 +1200

    Day 3 (maybe?)

--- Sources/SwiftPlayground/Interface.swift
+++ Sources/SwiftPlayground/Interface.swift
@@ -102,14 +102,16 @@ struct Interface {
         }
         clear()
         print("Thank you for entering your vehicals information\nWe will now work on getting it into the database, have fun!")
-        vehicles.append(Vehicle(displayName: vals[0].1,
-                                disctription: vals[1].1,
-                                model: vals[2].1,
-                                licencePlateNumber: vals[3].1,
-                                insuranceCost: Double(vals[4].1) ?? 404,
-                                vehicalCost: Double(vals[5].1) ?? 404,
-                                vehicalAge: Double(vals[6].1) ?? 404,
-                                currentFuelLevel: Double(vals[7].1) ?? 404))
+        vehicles.append(Vehicle(
+            id: String(Int.random(in: 100...999)), // A random number for that vehicle
+            displayName: vals[0].1,
+            disctription: vals[1].1,
+            model: vals[2].1,
+            licencePlateNumber: vals[3].1,
+            insuranceCost: Double(vals[4].1) ?? 404,
+            vehicalCost: Double(vals[5].1) ?? 404,
+            vehicalAge: Double(vals[6].1) ?? 404,
+            currentFuelLevel: Double(vals[7].1) ?? 404))
     }
     
     @MainActor public func removeVehicle() {
@@ -118,17 +120,69 @@ struct Interface {
         while valid == false{
             // Make sure that the screen is clear
             clear()
+            // Return if the list is empty
+            if (vehicles.isEmpty) {return}
+            
             print("What vehicle do you want to remove:")
             // Display a list of vehicles that they can remove
             for vehicle in vehicles {
                 print("\((vehicles.firstIndex(where: {$0.displayName == vehicle.displayName}) ?? 0) + 1) -- \(vehicle.displayName)") // Display it like "2 -- Name"
             }
             let pick = readLine()
-            if let p = pick {
-                if vehicles.count <= Int(p) ?? Int.max - 1{ // If its in range, the int.max is to make sure that it fails if its not in range
-                    // TODO: Continue
+            if let p: Int = Int(pick ?? "fail") {
+                if vehicles.count >= p && p > 0{ // If its in range
+                    clear()
+                    print("Removed \(vehicles[p - 1].displayName)")
+                    vehicles.remove(at: p - 1)
+                    valid = true
+                } else {
+                    valid = false
                 }
+            } else {
+                valid = false
             }
+        }
+    }
+    
+    @MainActor public func createRental(){
+        var valid1 = false
+        var valid2 = false
+        var vals = [
+            ("vehicle", "")
+        ]
+        // This is procidure now
+        while valid1 == false{
+            clear() // This is infront of everything at this point
+            
+            // This is going to use different code to the vehicle creator because it is picking from options
+            print("Please create your rental (you can enter q at any time to quit)\n")
+            print("First lets start with the vehicle that is being rented")
+            for vehicle in vehicles{
+                // This is the same as before for usability
+                print("\((vehicles.firstIndex(where: {$0.displayName == vehicle.displayName}) ?? 0) + 1) -- \(vehicle.displayName)")
+            }
+            let pick = readLine()
+            
+            if pick?.lowercased() == "q" {return} // Check if they want to cancel
+            
+            if let p: Int = Int(pick ?? "fail") {
+                if vehicles.count >= p && p > 0{ // If its in range
+                    clear()
+                    // Do a thing
+                    vals[0].1 = vehicles[p - 1].id
+                    print("Nice, lets now go on to the customer -->")
+                    valid1 = true
+                } else {
+                    valid1 = false
+                }
+            } else {
+                valid1 = false
+            }
+        }
+        
+        while valid2 == false {
+            print("Please pick a valid customer")
+            // TODO: Continue
         }
     }
 }
--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -12,11 +12,14 @@ import GRDB
 /// Vehical: Display Name, licence plate number, insurance cost, vehical cost, vehical age, model, engine specifications, current fuel level
 
 let ui: Interface = Interface()
+// TODO: Please make a cost for the rental
 
 /// Bogus information made with the use of large language model - gemini
 @MainActor var rentals: [Rental] = [
     Rental(
+        id: "964",
         customer: Customer(
+            id: "123",
             name: "Alex River",
             dob: dateFrom(year: 1992, month: 5, day: 14),
             bankCardNumber: 1234567890123456,
@@ -28,7 +31,9 @@ let ui: Interface = Interface()
         pickupDate: dateFrom(year: 2026, month: 5, day: 01)
     ),
     Rental(
+        id: "846",
         customer: Customer(
+            id: "575",
             name: "Jordan Smith",
             dob: dateFrom(year: 1985, month: 11, day: 22),
             bankCardNumber: 9876543210987654,
@@ -40,7 +45,9 @@ let ui: Interface = Interface()
         pickupDate: dateFrom(year: 2026, month: 4, day: 25)
     ),
     Rental(
+        id: "730",
         customer: Customer(
+            id: "464",
             name: "Sam Taylor",
             dob: dateFrom(year: 1998, month: 3, day: 05),
             bankCardNumber: 5544332211009988,
@@ -56,6 +63,7 @@ let ui: Interface = Interface()
 /// Another thing with bogus information, taken from gemini's list above
 @MainActor var vehicles: [Vehicle] = [
     Vehicle(
+        id: "374",
         displayName: "Silver Streak",
         disctription: "Luxury sedan with premium leather interior.",
         model: "BMW 5 Series",
@@ -66,6 +74,7 @@ let ui: Interface = Interface()
         currentFuelLevel: 65.5
     ),
     Vehicle(
+        id: "487",
         displayName: "The Mountain Mover",
         disctription: "A posh but rugged 4x4 with plenty of room for camping gear.",
         model: "Ford F-150",
@@ -76,6 +85,7 @@ let ui: Interface = Interface()
         currentFuelLevel: 120.0
     ),
     Vehicle(
+        id: "012",
         displayName: "Blue Lightning",
         disctription: "A fuel-efficient compact car perfect for city driving while looking fancy.", // And racist
         model: "Tesla Model 3",
@@ -95,6 +105,8 @@ struct SwiftPlayground {
             var pick = ui.printWelcome()
             if pick == "a" {
                 ui.addVehical()
+            } else if pick == "r"{
+                ui.removeVehicle()
             }
         }
     }
@@ -107,6 +119,7 @@ func dateFrom(year: Int, month: Int, day: Int) -> Date {
 
 public struct Vehicle {
     // All of the things needed for renting a car
+    public let id: String
     public var displayName: String
     public var disctription: String
     public let model: String
@@ -119,6 +132,7 @@ public struct Vehicle {
 }
 
 public struct Customer {
+    public let id: String
     public let name: String
     public let dob: Date
     public let bankCardNumber: Int
@@ -128,6 +142,7 @@ public struct Customer {
 }
 
 public struct Rental {
+    public let id: String
     public let customer: Customer
     public let vehicle: Vehicle
     public var returnDate: Date
